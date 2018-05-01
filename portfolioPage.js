9670c4e4427ac7e0dcd2fc515cd6f7c34a0b29bb
iAmThe1neAnd0nly <jmc31899@gmail.com>
2018-05-01 13:28:49 -0500

    added images to portfolio pages and restructured the home page

---
+++ portfolioPage.js
@@ -0,0 +1,34 @@
+var dir = "img/";
+
+var fileextension = ".png";
+
+console.log('js loaded');
+
+$(document).ready(function () {
+
+    if(getCurentFileName() == 'sketchbook.html'){
+        dir += "sketchbook/";
+    }
+    else if(getCurentFileName() == 'vizdev.html'){
+        dir += "vizDev/";
+    }
+    else if(getCurentFileName() == 'characterArt.html'){
+        dir+= "charArt/";
+    }
+
+    $.ajax({
+        url: dir,
+        success: function (data) {
+            $(data).find("a:contains(" + fileextension + ")").each(function () {
+                var filename = this.href.replace(window.location.host, "").replace("http:///", "");
+                $("#container").append($("<img src= " + dir + filename + "></img>"));
+            });
+        }
+    });
+});
+
+function getCurentFileName(){
+    var pagePathName= window.location.pathname;
+    return pagePathName.substring(pagePathName.lastIndexOf("/") + 1);
+}
+
